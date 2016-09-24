c626ed6c4f2184da66edd3588da4e8f8e8fd8b38
ArcyFlor <arcylizet@ucla.edu>
2016-09-24 11:14:19 -0700

    functioning application

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 var express = require('express');
-var bodyparser = require('body-parser');
+var bodyParser = require('body-parser');
 var connection = require('./connection');
 var routes = require('./routes');
+var path = require('path');
 
 var app = express();
-app.use(bodyparser.urlencoded({extended: true}));
-app.use(bodyparser.json());
-app.use('/', express.static(__dirname + '/public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
 
 connection.init();
 routes.configure(app);
 
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, 'home.html'));
+});
+
+app.get('/reserve', function (req, res) {
+	res.sendFile(path.join(__dirname, 'reserve.html'));
+});
+
+app.get('/tables', function (req, res) {
+	res.sendFile(path.join(__dirname, 'tables.html'));
+});
 
 var server = app.listen(8000, function() {
   console.log('Server listening on port ' + server.address().port);
 });
+
+
